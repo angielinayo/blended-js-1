e4ab5c8c2a5f246500fe90eaba71e43438478dba
Anhelina Shabrina <sh.angelina97@gmail.com>
2022-12-04 18:16:36 +0200

    task-7

--- index.js
+++ index.js
@@ -236,19 +236,47 @@
 //Збережіть його результат у змінній sum.
 //Якщо об'єкт salaries порожній, то результат має бути 0
 
-const salaries = {
-    Mango: 100,
-    Poly: 160,
-    Ajax: 1470,
-};
+// const salaries = {
+//     Mango: 100,
+//     Poly: 160,
+//     Ajax: 1470,
+// };
 
-function salariesSum(salaries) {
-    const salariesValue = Object.values(salaries);
-    let sum = 0;
-    for(const salary of salariesValue) {
-        sum += salary;
-    }
-    console.log(sum);
+// function salariesSum(salaries) {
+//     const salariesValue = Object.values(salaries);
+//     let sum = 0;
+//     for(const salary of salariesValue) {
+//         sum += salary;
+//     }
+//     console.log(sum);
+// }
+
+// salariesSum(salaries);
+
+
+/*
+? Напишіть функцію each(array, callback), яка першим параметром очікує масив, а другим - функцію, яка застосовується 
+до кожного елемента масиву.
+? Функція each повинна повернути новий масив, елементами якого будуть результати виклику колбека.
+*/
+
+function multi(number) {
+    return number * 21;
 }
 
-salariesSum(salaries);
+function each(array, callback) {
+    let result = [];
+
+    for(let i = 0; i < array.length; i += 1) {
+       const number = callback(array[i]);
+       console.log(number);
+       result.push(number);
+    }
+        return result;
+}
+
+const numbers = [1, 3, 4, 5];
+
+// console.log(each(numbers, function(number) {return number * 21}));
+
+console.log(each(numbers, (number => number * 21)));
